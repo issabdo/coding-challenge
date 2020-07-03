d4772a508cbd6e867c46e25a2ae60ff17461c2c8
Abdo <issabdo0011@gmail.com>
2020-07-03 12:57:49 +0100

    Create relation Product_Category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function categorys()
+    {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
 }
