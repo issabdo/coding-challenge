235781b7a419d33e3a15ecf9f87ab80166272408
Abdo <issabdo0011@gmail.com>
2020-07-04 03:54:14 +0100

    Create ProductTest unit

--- app/Product.php
+++ app/Product.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class Product extends Model
+class Product extends Model implements Authenticatable
 {
+    use AuthenticableTrait;
+
     public function categorys()
     {
         return $this->belongsToMany(Category::class)->withTimestamps();
